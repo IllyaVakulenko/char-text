158c268d4017a10ed2e3f3ea4785091d1181596d
Illya Vakulenko <vacodenko@gmail.com>
2024-10-20 11:40:49 +0300

    Add basic functional

---
+++ main.py
@@ -0,0 +1,30 @@
+char_map = {
+    "0": [[0, 0, 1, 1, 1, 0, 0],
+          [0, 1, 0, 0, 0, 1, 0],
+          [0, 1, 0, 0, 1, 1, 0],
+          [0, 1, 0, 1, 0, 1, 0],
+          [0, 1, 1, 0, 0, 1, 0],
+          [0, 1, 0, 0, 0, 1, 0],
+          [0, 0, 1, 1, 1, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0]]
+}
+
+THEME = {
+    "background": " ",
+    "foreground": "#"
+}
+
+bg = THEME["background"]
+fg = THEME["foreground"]
+
+
+def print_char(char_matrix):
+    for row in char_matrix:
+        line = ''.join(fg if pixel else bg for pixel in row)
+        print(line)
+
+
+text = "0"
+
+for char in text:
+    print_char(char_map[char])
